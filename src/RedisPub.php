e4c6f4a75d2cf241733f4950d993d04d2b34a8f1
Hosea Kibet <iankibet@Hoseas-MacBook-Air.local>
2024-11-24 13:59:19 +0300

    added redispub

---
+++ src/RedisPub.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Iankibet\RedisSub;
+
+use Illuminate\Support\Facades\Redis;
+
+class RedisPub
+{
+    public static function publish(string $channel, $payload): void
+    {
+        $message = json_encode($payload);
+        $redis = Redis::connection(config('redis-sub.connection'));
+        $redis->publish($channel, $message);
+    }
+}
